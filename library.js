f6fdd8f3b7a0d30165da2996d6568cec60034a31
Gary Arzumanyan <garyarzumanyan@gmail.com>
2020-12-13 22:41:39 -0800

    Added cards and formatted with CSS

--- library.js
+++ library.js
@@ -1,11 +1,9 @@
-
 //When user Presses buttons//
 document.querySelector('.newBookButton').addEventListener('click', newBookPopUp); 
 document.querySelector('.closeButton').addEventListener('click', closePopUp); 
 document.querySelector('.submitButton').addEventListener('click', handleFormInput); 
 
 let myLibrary = [];
-
 
 //book object definition
 function Book(title, author, numOfPages, haveRead){
@@ -14,10 +12,10 @@ function Book(title, author, numOfPages, haveRead){
     this.numOfPages = numOfPages;
     this.haveRead = haveRead;
     this.info = function(){
-      const infoText = `${title} by ${author}, ${numOfPages} pages, ${haveRead === "on" ? "have read":"not read yet"}`
-      return infoText;
+        const infoText = `${title} by ${author}, ${numOfPages} pages, ${haveRead === "on" ? "have read":"not read yet"}`
+        return infoText;
     }
-  }
+}
 
 function addBookToLibrary(title, author, numOfPages, haveRead){
     const myBook = new Book(title, author, numOfPages, haveRead);
@@ -26,26 +24,38 @@ function addBookToLibrary(title, author, numOfPages, haveRead){
 
 //Sample books
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "on");
-addBookToLibrary("The Hobbit", "234234", 295, "off");
-addBookToLibrary("The Hobbit", "iran", 295, "off");
-addBookToLibrary("The Hobbit", "killer", 295, "on");
-addBookToLibrary("The Hobbit", "fred", 295, "off");
-
+addBookToLibrary("Harry Potter", "JK Rowling", 490, "on");
+addBookToLibrary("Dune", "Hamilton", 990, "off");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "on");
+addBookToLibrary("Harry Potter", "JK Rowling", 490, "on");
+addBookToLibrary("Dune", "Hamilton", 990, "off");addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "on");
+addBookToLibrary("Harry Potter", "JK Rowling", 490, "on");
+addBookToLibrary("Dune", "Hamilton", 990, "off");
 
 function displayCurrentBooks(){
     myLibrary.forEach(element => createBookCard(element));
 }
 
 function createBookCard(myBook){
-    myDivNode = document.createElement('div');
-
-    myDivNode.className = "bookCard";
-    myDivNode.setAttribute('data-array', myLibrary.indexOf(myBook)); //sets data-array attribute for each book to the index it is in the myLibrary array;
-    myDivNode.textContent = myBook.info();
-    //remove
-    console.log(myDivNode);
-    //remove
-    document.querySelector("body").appendChild(myDivNode);
+    //creates Card div and sets up the data-array attribute to hold the location of the book in the myLibrary array
+    myCardNode = document.createElement('div');
+    myCardNode.className = "bookCard";
+    myCardNode.setAttribute('data-array', myLibrary.indexOf(myBook)); //sets data-array attribute for each book to the index it is in the myLibrary array;
+    document.querySelector(".cardContainer").appendChild(myCardNode);
+   
+    //creates Divs for the title, author, pages;
+    for(prop in myBook){
+        let myDivNode = document.createElement('div');
+        if (prop === "title" || prop === "author" || prop === "numOfPages"){
+            myDivNode.className = prop;
+            myDivNode.textContent = myBook[prop];
+            document.querySelector(`[data-array="${myLibrary.indexOf(myBook)}"]`).appendChild(myDivNode);
+        }
+        if (prop === "numOfPages"){
+            myDivNode.textContent += " pages";
+        }
+    }
+    
 }
 
 function handleFormInput(e){
@@ -58,11 +68,12 @@ function handleFormInput(e){
     )
 
     closePopUp();
-    createBookCard(myLibrary[myLibrary.length-1]);
+    createBookCard(myLibrary[myLibrary.length-1]); //Creates book card for the book we just added to array which would be the last one, hence length - 1;
 }
+
 //open new book addition menu popup
 function newBookPopUp(){
-    document.getElementById("myForm").style.display = "flex";
+    document.getElementById("myForm").style.display = "block";
 }
 
 //close new book addition menu popup without submitting
